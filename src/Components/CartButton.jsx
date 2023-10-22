06206d0962361f353c86205e2f92029d49b4ec76
faisalach <faisalachramsyah@gmail.com>
2023-10-22 12:49:44 +0700

    rapihin homeMenu (Codingan card per kategori, centerin card, button keranjang di pojok bawah)

--- src/Components/CartButton.jsx
+++ src/Components/CartButton.jsx
@@ -2,8 +2,10 @@ import { BsCartFill } from "react-icons/bs";
 
 function CartButton() {
   return (
-    <div className="flex items-center justify-center fixed bottom-4 right-4 sm:bottom-4 md:right-1/4 md:-translate-x-60 bottom-4 md:right-1/4 md:-translate-x-60 w-14 h-14 rounded-full bg-[#98694F] shadow-lg">
-      <BsCartFill size={25} className="text-white" />
+    <div className="bg-slate-200 max-w-lg h-screen fixed top-0" style={{ transform: 'translate(-50%,0)',left:"50%" }}>
+      <div className="flex items-center justify-center absolute bottom-4 w-14 h-14 rounded-full bg-[#98694F] shadow-lg" style={{ transform: "translate(170px)" }}>
+        <BsCartFill size={25} className="text-white" />
+      </div>
     </div>
   );
 }
