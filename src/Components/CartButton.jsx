71ac7416898cbb2ec761e3633220f1d1d800f3ae
faisalach <faisalachramsyah@gmail.com>
2023-10-31 09:11:54 +0700

    fix card button for mobilephone

--- src/Components/CartButton.jsx
+++ src/Components/CartButton.jsx
@@ -2,7 +2,7 @@ import { BsCartFill } from "react-icons/bs";
 
 function CartButton() {
   return (
-    <div className="bg-slate-200 max-w-lg h-screen fixed top-0" style={{ transform: 'translate(-50%,0)',left:"50%" }}>
+    <div className="h-screen fixed card-button-container">
       <div className="flex items-center justify-center absolute bottom-4 w-14 h-14 rounded-full bg-[#98694F] shadow-lg md:translate-x-44 sm:translate-x-36 translate-x-20">
         <BsCartFill size={25} className="text-white" />
       </div>
