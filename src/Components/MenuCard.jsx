ecefadb626c77db2a0ef88c97db1710f7753340e
mkp-kemal <kemal.garut7@gmail.com>
2023-11-01 13:56:07 +0700

    render menu

--- src/Components/MenuCard.jsx
+++ src/Components/MenuCard.jsx
@@ -1,33 +1,91 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import PlusMinusButton from "./PlusMinusButton";
 import { BsFillCartPlusFill } from "react-icons/bs";
+import { useEffect, useState } from "react";
+import axios, { all } from "axios";
 
 function MenuCard() {
+  const [isFixed, setIsFixed] = useState(false);
+
+  // buat handle card menu pas ngescroll jdi geser kebawah dikit
+
+  const handleScroll = () => {
+    if (window.scrollY > 230) {
+      setIsFixed(true);
+    } else {
+      setIsFixed(false);
+    }
+  };
+
+  window.addEventListener("scroll", handleScroll);
+
+
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const reserve = params.get("reserve");
+
+
+  const [allProducts, setAllProducts] = useState([]);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/api/all-product');
+      setAllProducts(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="card mt-5 sm:w-48 md:w-60 flex justify-center">
-      <div className="card-body flex flex-col bg-white p-3 ml-2 mr-2 xs:ml-7 xs:mr-7 md:ml-4 md:mr-4 rounded-2xl shadow-xl ">
-        <Link to="/product-detail" className="cursor-pointer">
-          <img src="/latte.png" alt="" className="w-full" />
-          <h3 className="text-md sm:text-lg text-[#414141] font-bold">
-            Cafe Latte
-          </h3>
-        </Link>
-        <p className="text-xs sm:text-md text-[#414141]">
-          Espresso dengan susu, harmoni kopi kaya dan kelembutan susu dalam
-          setiap tegukan asikkkk
-        </p>
-        <div className="flex flex-row justify-between mt-2  items-center">
-          <p className="text-xs sm:text-md text-[#98694F] font-bold pr-1 sm:pr-0">
-            17.000
-          </p>
-          <PlusMinusButton />
-          <BsFillCartPlusFill
-            size={20}
-            className=" text-[#98694F] my-auto pl-1 sm:pl-0"
-          />
-        </div>
-      </div>
+    <div
+          className={`flex flex-row flex-wrap ${
+            isFixed ? `mt-24 transition duration-500 ease-in-out pb-32` : ""
+          }`}
+          style={{ justifyContent: "center" }}
+        >
+      {
+        allProducts.map((items) => {
+          return (
+            <div className="flex flex-row flex-wrap justify-center w-full">
+              <h1 className="ms-8 w-full">{items.nama_kategori_menu}</h1>
+              {
+                items.menu.map((menu) => {
+                  return (
+                    <div className="card mt-5 sm:w-48 md:w-60 flex justify-center">
+                      <div className="card-body flex flex-col bg-white p-3 ml-2 mr-2 xs:ml-7 xs:mr-7 md:ml-4 md:mr-4 rounded-2xl shadow-xl ">
+                        <Link to="/product-detail" className="cursor-pointer">
+                          <img src="/latte.png" alt="" className="w-full" />
+                          <h3 className="text-md sm:text-lg text-[#414141] font-bold">
+                            {menu.nama_menu}
+                          </h3>
+                        </Link>
+                        <p className="text-xs sm:text-md text-[#414141]">
+                          Espresso dengan susu, harmoni kopi kaya dan kelembutan susu dalam
+                          setiap tegukan asikkkk
+                        </p>
+                        <div className="flex flex-row justify-between mt-2  items-center">
+                          <p className="text-xs sm:text-md text-[#98694F] font-bold pr-1 sm:pr-0">
+                            17.000
+                          </p>
+                          <PlusMinusButton />
+                          <BsFillCartPlusFill
+                            size={20}
+                            className=" text-[#98694F] my-auto pl-1 sm:pl-0"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          )
+        })
+      }
     </div>
+
   );
 }
 
--- src/Pages/homeMenu.jsx
+++ src/Pages/homeMenu.jsx
@@ -34,26 +34,8 @@ function HomeMenu() {
           </p>
         </div>
         <CategorySection />
-        <div
-          className={`flex flex-row flex-wrap ${
-            isFixed ? `mt-24 transition duration-500 ease-in-out pb-32` : ""
-          }`}
-          style={{ display: "flex", justifyContent: "center" }}
-        >
-          <div id="kopi">
-            <MenuCard />
-          </div>
-          <div id="minuman" className="flex flex-row flex-wrap justify-center">
-            <MenuCard />
-            <MenuCard />
-            <MenuCard />
-            <MenuCard />
-          </div>
-          <div id="cemilan" className="flex flex-row flex-wrap justify-center">
-            <MenuCard />
-            <MenuCard />
-          </div>
-        </div>
+        
+          <MenuCard />
         <Link to="/checkout-form">
           <CartButton inputClassname="flex items-center justify-center fixed bottom-4 right-4 sm:bottom-4 md:right-4 lg:right-1/4 lg:-translate-x-32 xl:right-1/4 xl:-translate-x-56 w-14 h-14 rounded-full bg-[#98694F] shadow-lg cursor-pointer" />
         </Link>
