f9ff417fff23257d0b10cd3c00b126c869288b76
faisalach <faisalachramsyah@gmail.com>
2023-11-02 15:03:30 +0700

    fix scroll category on mobile

--- src/Components/CategorySection.jsx
+++ src/Components/CategorySection.jsx
@@ -21,13 +21,14 @@ function CategorySection() {
       {/* <h3 className="text-md sm:text-lg font-bold text-[#414141]">Kategori</h3> */}
       <div
         // state isFixed dipake di sini
-        className={`flex gap-5 py-3 transition duration-300 md:transition-none overflow-x-auto scroll-smooth scrollbar-hide ${
+        className={` w-full ${
           isFixed
             ? "fixed top-0 left-0 md:top-0 md:left-1/2 md:-translate-x-1/2 bg-white px-7 transition duration-500 md:transition-none"
             : ""
         }`}
         style={{ zIndex: 100 }}
       >
+        <div className="flex gap-5 py-3 transition duration-300 md:transition-none overflow-x-auto scroll-smooth scrollbar-hide">
         <Link to="Kopi" smooth={true} duration={500} offset={-80}>
           <ListCategory imgPath="/coffe.png" text="Kopi" />
         </Link>
@@ -46,6 +47,7 @@ function CategorySection() {
         <Link to="">
           <ListCategory imgPath="/bibimbap.png" text="Makanan" />
         </Link>
+        </div>
       </div>
       <div
         // state isFixed dipake di sini
