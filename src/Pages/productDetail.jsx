c7dded7a661277406bdada6d49ad80998b465e53
faisalach <faisalachramsyah@gmail.com>
2023-11-03 08:36:50 +0700

    product detail data

--- src/Pages/productDetail.jsx
+++ src/Pages/productDetail.jsx
@@ -1,15 +1,42 @@
 import { BsArrowLeft, BsCartFill, BsFillCartPlusFill } from "react-icons/bs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PlusMinusButton from "../Components/PlusMinusButton";
+import axios from "axios";
+import { Helper } from "../Helper/Helper";
+import Loading from "../Components/Loading";
 
 function ProductDetail() {
   const [count, setCount] = useState(0);
   const location = useLocation();
   const params = new URLSearchParams(location.search);
   const reserve = params.get("reserve");
+  const id_menu = params.get("menu");
+  const [isLoading, setIsLoading] = useState(true);
+  const { formatRupiah , baseURLAPI} = Helper();
+
+  // GET PRODUCTS
+  const [product, setProduct] = useState([]);
+  const fetchDataMenu = async () => {
+    try {
+      const response = await axios.get(baseURLAPI(`menu/${id_menu}`));
+      setProduct(response.data[0]);
+
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 500);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    fetchDataMenu();
+  }, []);
+
+  console.log(product);
+
 
   // TOAST SUKSES
   const showToastSuccess = (msg) => {
@@ -28,7 +55,7 @@ function ProductDetail() {
     <div>
       <ToastContainer />
       <div className="bg-warnaBg mx-auto my-0 max-w-lg h-full pb-8">
-        <div className="flex justify-between items-center  bg-[#98694F] px-7 py-5">
+        <div className="flex justify-between items-center bg-[#98694F] px-7 py-5">
           <Link to="/">
             <BsArrowLeft size={30} className="z-50 text-white" />
           </Link>
@@ -38,41 +65,48 @@ function ProductDetail() {
             <BsCartFill size={25} className="text-white" />
           </Link>
         </div>
-        <img
-          src="/latte2.png"
-          alt=""
-          className="w-full"
-        />
-        <div className="absolute inset-x-0 top-[330px] sm:top-[360px] md:top-[450px] flex items-center justify-center">
-          <PlusMinusButton variant="detailMenu" />
-        </div>
-
-        <div className="max-w-[80%] bg-white mx-auto pb-[115px]">
-          <div className="flex flex-row justify-between p-5">
-            <h3 className="text-md sm:text-lg text-[#414141] font-bold">
-              Cafe Latte
-            </h3>
-            <p className="text-md sm:text-lg text-[#98694F] font-bold ">
-              Rp. 17.000
-            </p>
+        {isLoading ? (
+          <div className="flex justify-center items-center h-screen">
+            <Loading/>
           </div>
-          <p className="p-5 text-sm h-full">
-            Cafe latte, sering disebut hanya sebagai adalah minuman kopi
-            berbasis espresso yang populer dan klasik yang berasal dari Italia
-            dan telah mendapatkan popularitas luas di seluruh dunia dunia. Nama
-            LATTE berasal dari kata Italia untuk susu,dan minuman ini ditandai
-            dengan tekstur yang halus dan krim.
-          </p>
-        </div>
-        <button
-          onClick={() => {
-            showToastSuccess("Hidangan ditambahkan");
-          }}
-          className="flex flex-row justify-between p-4 w-[80%] mx-auto mt-6  bg-[#98694F] rounded-lg "
-        >
-          <h3 className="text-white font-semibold">Tambah Ke Keranjang</h3>
-          <BsFillCartPlusFill size={30} className="text-white" />
-        </button>
+        ) : (
+          <div >
+            <img
+              src={product.url_gambar}
+              alt=""
+              className="w-full object-cover"
+              style={{aspectRatio : 3/2}}
+            />
+            <div className="relative  mb-14">
+              <div className="absolute inset-x-0 top-[-20px] flex items-center justify-center">
+                <PlusMinusButton variant="detailMenu" />
+              </div>
+            </div>
+
+            <div className="max-w-[80%] bg-white mx-auto pb-[115px]">
+              <div className="flex flex-row justify-between p-5">
+                <h3 className="text-md sm:text-lg text-[#414141] font-bold">
+                  {product.nama_menu}
+                </h3>
+                <p className="text-md sm:text-lg text-[#98694F] font-bold ">
+                  {formatRupiah(product.harga_menu)}
+                </p>
+              </div>
+              <p className="p-5 text-sm h-full">
+                {product.deskripsi_menu}
+              </p>
+            </div>
+            <button
+              onClick={() => {
+                showToastSuccess("Hidangan ditambahkan");
+              }}
+              className="flex flex-row justify-between p-4 w-[80%] mx-auto mt-6  bg-[#98694F] rounded-lg "
+            >
+              <h3 className="text-white font-semibold">Tambah Ke Keranjang</h3>
+              <BsFillCartPlusFill size={30} className="text-white" />
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
